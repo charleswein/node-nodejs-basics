96177db4ce8d54378282d0aa3dd1c495b41e6a0f
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-12-04 13:17:32 +0400

    feat: practice with read and write stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import {join} from "node:path";
+import {createReadStream} from "node:fs";
+import {getPathToFolders} from "../utils.mjs";
+import {pipeline} from "node:stream/promises";
+
+const {pathToFilesFolder} = getPathToFolders(import.meta.url)
+
 const read = async () => {
-    // Write your code here 
+    // Write your code here
+    const rs = createReadStream(join(pathToFilesFolder, '/fileToRead.txt'), {encoding: "utf-8"})
+
+    try {
+        await pipeline(rs, process.stdout)
+    } catch (err) {
+        throw err
+    }
 };
 
 await read();
