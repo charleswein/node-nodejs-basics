830009a00e50825a1fe4732c2fc1e390d7dad31e
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-12-04 13:05:30 +0400

    feat: implement read file function, list folder function, rename file function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import {getPathToFolders, operationFailedMessage} from "../utils.mjs";
+import {readdir} from "node:fs/promises";
+
+const {pathToFilesFolder} = getPathToFolders(import.meta.url)
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        const files = await readdir(pathToFilesFolder)
+
+        files.forEach(value => console.log(value))
+    } catch (err) {
+        if(err.code === "ENOENT") {
+            throw new Error(operationFailedMessage)
+        }
+        throw err
+    }
 };
 
 await list();
