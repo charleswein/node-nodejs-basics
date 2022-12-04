074d2569fe2a34b985bf632b336b26786ce03e2d
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-12-04 13:07:15 +0400

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import {readFile} from "node:fs/promises";
+import {join} from "node:path";
+import {createHash} from "node:crypto";
+import {getPathToFolders} from "../utils.mjs";
+
+const {pathToFilesFolder} = getPathToFolders(import.meta.url)
+
 const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const file = await readFile(join(pathToFilesFolder, '/fileToCalculateHashFor.txt'))
+    console.log(createHash('sha256').update(file).digest('hex'));
 };
 
 await calculateHash();
