52f2c9651de42066ef22965d44daa29c0f7e3e42
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-12-04 12:37:06 +0400

    feat: cli implement parseArgs and parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+    // Write your code here
+    const arr = []
+    Object.keys(process.env).forEach((key) => {
+        if(key.startsWith("RSS_")) {
+            arr.push(`${key}=${process.env[key]}`);
+        }
+    })
+
+    console.log(arr.join('; '))
 };
 
 parseEnv();
