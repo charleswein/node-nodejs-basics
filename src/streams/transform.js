54f445ea8534b669947057501e9988c30e4493f6
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-12-04 13:20:50 +0400

    feat: practice with transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+import {Transform} from "node:stream";
+import {EOL} from "node:os";
+import {pipeline} from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+    // Write your code here
+    const ts = new Transform({
+        transform(chunk, encoding, callback) {
+            const str = chunk.toString().trim().split('').reverse().join('')
+            // this.push(str)
+            callback(null, str + EOL)
+        }
+    })
+
+    try {
+        await pipeline(process.stdin, ts, process.stdout)
+    } catch (err) {
+        throw err
+    }
 };
 
 await transform();
